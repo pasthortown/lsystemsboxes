0d65895f42c6422e57e44528e29b75cd5c4319f5
pasthortown <pastorchessmaster@gmail.com>
2018-10-23 08:26:27 -0500

    ok

---
+++ BDD/2. Uniques.sql
@@ -0,0 +1,8 @@
+ALTER TABLE Caja ADD UNIQUE unique_index_caja (codigo);
+ALTER TABLE EstadoCaja ADD UNIQUE unique_index_estado_caja (idCaja,fecha,idEstado);
+ALTER TABLE Estado ADD UNIQUE unique_index_estado (descripcion);
+ALTER TABLE Reciclaje ADD UNIQUE unique_index_reciclaje (idCaja,fecha,idUsuario,cantidad);
+ALTER TABLE Rol ADD UNIQUE unique_index_rol (descripcion);
+ALTER TABLE Usuario ADD UNIQUE unique_index_usuario (identificacion, email);
+ALTER TABLE Cuenta ADD UNIQUE unique_index_cuenta (idUsuario, idRol, nickname);
+ALTER TABLE AvatarUsuario ADD UNIQUE unique_index_avatar_usuario (idUsuario);
